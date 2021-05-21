51d03757534fc17d6cbab8c053fd53bd35b69aaf
vishnu_jayan <vishnujayadhevan@yahoo.com>
2021-05-21 21:18:56 +0530

    Removed footer language content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,16 +8,6 @@ class Footer extends React.Component {
             <footer id="footer">
                 <div className="container">
                     <div className="copyright">
-                    <div className="lang">
-                        <select name="language" >
-                            <option value="en">English</option>
-                            <option value="hi">Hindi</option>
-                            <option value="ml">Malayalam</option>
-                            <option value="ta">Tamil</option>
-                            <option value="mr">Marathi</option>
-                        </select>
-                        <a className="active" href="example.com">EN</a>/<a href="example.com">ML</a>
-                    </div>
                     © Copyright <a target="_blank" href="https://ctara.iitb.ac.in" rel="noreferrer"></a><strong><span>CTARA</span></strong>
                     </div>
                     <div className="credits">
