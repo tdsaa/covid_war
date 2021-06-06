142c74d36a43ed342c621fc768cf723015507e7a
vishnu_jayan <vishnujayadhevan@yahoo.com>
2021-06-06 15:47:24 +0530

    added bolded text

--- src/Content/mental_health.js
+++ src/Content/mental_health.js
@@ -9,53 +9,65 @@ const mental_health = {
             'contents': [
                 {
                     'id': 1,
-                    'value':'Take breaks from watching, reading, or listening to news stories, including those on social media: It’s good to be informed, but hearing about the pandemic constantly can be upsetting. Consider limiting news to just a couple times a day and disconnecting from phone, TV, and computer screens for a while. Avoid watching news right before you sleep.',
+                    'value':'Take breaks from watching, reading, or listening to news stories, including those on social media:',
+                    'detail': 'It’s good to be informed, but hearing about the pandemic constantly can be upsetting. Consider limiting news to just a couple times a day and disconnecting from phone, TV, and computer screens for a while. Avoid watching news right before you sleep.',
                 },
                 {
                     'id': 2,
-                    'value':'Avoid fake news: Whenever you’re on social media, think of your own and others’ mental health. Think about why and how you’re using it. Is it benefiting you, someone else or is it just mindless scrolling? Be analytical and only share something when you’ve verified the source. Sharing fake news is so disturbing, it has a negative impact on all of us, especially on those with mental health problem.',
+                    'value':'Avoid fake news: ',
+                    'detail': 'Whenever you’re on social media, think of your own and others’ mental health. Think about why and how you’re using it. Is it benefiting you, someone else or is it just mindless scrolling? Be analytical and only share something when you’ve verified the source. Sharing fake news is so disturbing, it has a negative impact on all of us, especially on those with mental health problem.',
                 },
 
                 {
                     'id': 3,
-                    'value':'Manage your expectations: Don’t expect to have very high productivity since you are working from home. Do not underestimate the cognitive and emotional load that this pandemic brings, or the impact it will have on your productivity. Difficulty concentrating, low motivation and a state of distraction are to be expected. Adaptation will take time. Go easy on yourself others in your charge. ',
+                    'value':'Manage your expectations:',
+                    'detail': ' Don’t expect to have very high productivity since you are working from home. Do not underestimate the cognitive and emotional load that this pandemic brings, or the impact it will have on your productivity. Difficulty concentrating, low motivation and a state of distraction are to be expected. Adaptation will take time. Go easy on yourself others in your charge. ',
                 },
 
                 {
                     'id': 4,
-                    'value':'Routine is your friend: It helps to manage anxiety, and will help you to adapt more quickly to this current reality. Create clear distinctions between work and non-work time, ideally in both your physical workspace and your head space. Find something to do that is not work and is not virus-related that brings you joy. Working in short bursts with clear breaks will help to maintain your clarity of thought',
+                    'value':'Routine is your friend:',
+                    'detail': ' It helps to manage anxiety, and will help you to adapt more quickly to this current reality. Create clear distinctions between work and non-work time, ideally in both your physical workspace and your head space. Find something to do that is not work and is not virus-related that brings you joy. Working in short bursts with clear breaks will help to maintain your clarity of thought',
                 },
                 {
                     'id': 5,
-                    'value':'Manage uncertainty by staying in the present: Take each day as it comes and focus on the things you can control. Do not try to imagine the future or try to compare situations with pre-pandemic past.',
+                    'value':'Manage uncertainty by staying in the present:',
+                    'detail': ' Take each day as it comes and focus on the things you can control. Do not try to imagine the future or try to compare situations with pre-pandemic past.',
                 },
                 {
                     'id': 6,
-                    'value':"Answer any what-ifs:Answering your questions can help you establish plans you never thought you needed. If you know exactly who you'd call and exactly where you'd go if you did get very sick, this might help you feel less nervous about the hypothetical scenario.",
+                    'value':"Answer any what-ifs:",
+                    'detail': "Answering your questions can help you establish plans you never thought you needed. If you know exactly who you'd call and exactly where you'd go if you did get very sick, this might help you feel less nervous about the hypothetical scenario.",
                 },
                 {
                     'id': 7,
-                    'value':'Focus on what you can control: We have to focus on things which are in our control rather than worrying about things those are not. If you worry about getting exposed to the coronavirus every time you leave your house, try changing your focus to what you can do rather than what you have no control over. For instance, you can stay ten feet from others, you can wear a mask, and you can stay home as much as possible to avoid unnecessary contact.',
+                    'value':'Focus on what you can control:',
+                    'detail': ' We have to focus on things which are in our control rather than worrying about things those are not. If you worry about getting exposed to the coronavirus every time you leave your house, try changing your focus to what you can do rather than what you have no control over. For instance, you can stay ten feet from others, you can wear a mask, and you can stay home as much as possible to avoid unnecessary contact.',
                 },
                 {
                     'id': 8,
-                    'value':'Know your red flags: One way to manage moments of distress is to identify key thoughts or physical sensations that tend to contribute to your cycle of distress and feelings of being overwhelmed. ',
+                    'value':'Know your red flags:',
+                    'detail': ' One way to manage moments of distress is to identify key thoughts or physical sensations that tend to contribute to your cycle of distress and feelings of being overwhelmed. ',
                 },
                 {
                     'id': 10,
-                    'value':'Be compassionate with yourself and with others: Do not attach the COVID with any ethnicity or nationality. Be empathetic to all those who are affected. People who are affected have not done anything wrong and they deserve our support, compassion and kindness. This pandemic will cause a lot of stress for many of us, and we cannot be our best selves all the time. But we can ask for help or reach out when help is asked of us.',
+                    'value':'Be compassionate with yourself and with others:',
+                    'detail': ' Do not attach the COVID with any ethnicity or nationality. Be empathetic to all those who are affected. People who are affected have not done anything wrong and they deserve our support, compassion and kindness. This pandemic will cause a lot of stress for many of us, and we cannot be our best selves all the time. But we can ask for help or reach out when help is asked of us.',
                 },
                 {
                     'id': 11,
-                    'value':'Maintain connections: Even the most introverted of us need some sense of connection to others for our mental as well as our physical health. Create virtual forums to maintain connections with your friends, colleagues and families. We are in social isolation, but we need not feel alone. Reach out to those who might be particularly isolated',
+                    'value':'Maintain connections:',
+                    'detail': ' Even the most introverted of us need some sense of connection to others for our mental as well as our physical health. Create virtual forums to maintain connections with your friends, colleagues and families. We are in social isolation, but we need not feel alone. Reach out to those who might be particularly isolated',
                 },
                 {
                     'id': 12,
-                    'value':'Proactively Manage your stress threshold:Lay a solid foundation for your mental health and well-being by prioritizing your sleep, and practice good sleep hygiene, Eat well, Exercise, Avoid excessive alcohol, tobacco, and substance use. This will lower your stress levels, help you to better regulate your emotions',
+                    'value':'Proactively Manage your stress threshold:',
+                    'detail': 'Lay a solid foundation for your mental health and well-being by prioritizing your sleep, and practice good sleep hygiene, Eat well, Exercise, Avoid excessive alcohol, tobacco, and substance use. This will lower your stress levels, help you to better regulate your emotions',
                 },
                 {
                     'id': 13,
-                    'value':'Look for the inspirational stories during the pandemic:COVID has presented an unique challenge in front of the humanity. In this situation we are witnessing many inspirational and brave stories of people helping one another. Learning about these warriors and their work during the pandemic can help us stay motivated and will give us confidence to survive through this situation',
+                    'value':'Look for the inspirational stories during the pandemic:',
+                    'detail': 'COVID has presented an unique challenge in front of the humanity. In this situation we are witnessing many inspirational and brave stories of people helping one another. Learning about these warriors and their work during the pandemic can help us stay motivated and will give us confidence to survive through this situation',
                 },
 
             ]
